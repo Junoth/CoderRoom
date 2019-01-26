ebd7c0aa77d082ebf06060f5695b4eac0a854aaa
junoth <heathmyh@gmail.com>
2019-01-26 16:33:04 -0500

    Nginx configuration

---
+++ build/precache-manifest.d5b89f4247eba61e0b6e7d18a4bcfb7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32c6eab6d6479f7dce5bd1dfe532453",
+    "url": "/static/media/background.c32c6eab.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2c55be844bd45cc0d95",
+    "url": "/static/js/main.e2c55be8.chunk.js"
+  },
+  {
+    "revision": "5b5bb51718c75ad5ddea",
+    "url": "/static/js/1.5b5bb517.chunk.js"
+  },
+  {
+    "revision": "e2c55be844bd45cc0d95",
+    "url": "/static/css/main.038e7734.chunk.css"
+  },
+  {
+    "revision": "5b5bb51718c75ad5ddea",
+    "url": "/static/css/1.2af2731b.chunk.css"
+  },
+  {
+    "revision": "8af3c2dc9c9398b223c018163ce57902",
+    "url": "/index.html"
+  }
+];
